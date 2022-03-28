36b3c343e93d7063685220e888fba4e46183b0dc
Katy Brown <katybr35@gmail.com>
2022-03-28 13:23:31 -0500

    working on event listener and console log for hourblocks

--- script.js
+++ script.js
@@ -9,22 +9,57 @@ moment().hour(Number);
 // moment().fromNow();
 // var hourEl = document.getElementById("description");
 // var time = moment().format("hh:mm:ss");
-// var rows = 
+// var rows =
 // $("hour").text(time);
-function all(){
+//function all() {
+  // //console.log(this);
+  // var time = $(this).parent().attr("id");
+  // var text = $(this).siblings().val();
 
-var storedDescriptionEl = $(".description");
-var hourEl = $(".hour");
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var storedDescriptionEl = $(this).siblings(".description");
+    //var hourEl = $(".hour");
 
+    let input = storedDescriptionEl.val();
+    console.log("text: " ,input);
 
-$(".saveBtn").on("click", function (event) {
-  event.preventDefault();
-  console.log("button clicked");
-  localStorage.setItem("input", JSON.stringify(storedDescriptionEl));
-})
-  
+    var time = $(this).parent().attr("id");
+    console.log("time: " + time);
+    console.log("button clicked");
+    // localStorage.setItem("input", JSON.stringify(".description"));
+
+    // $("8.description").val(localStorage.getItem("8"));
+  });
+
+  function plannerHours() {
+    //var currentHour: calculated from momentJS
+    var currentHour = moment().hour();
+    //".hourBlock" pulled in from HTML
+    //runs constantly through the hour blocks
+    $(".hourBlock").each(function () {
+      //converts the string into an integer and assigns an attribute and splits the array
+      var hourBlockEl = parseInt($(this).attr("id").split("hour")[1]);
+      console.log(currentHour, hourBlockEl);
+
+      if (hourBlockEl < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      } else if (hourBlockEl === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+      } else if (hourBlockEl > currentHour) {
+        $(this).addClass("future");
+        $(this).addClass("past");
+        $(this).addClass("present");
+      }
+    });
+  }
+
+  plannerHours();
   //storedDescriptionEl = localStorage.getItem(JSON.stringify( storedDescriptionEl));
   //console.log(storedDescriptionEl);
-
-}
-all ();
+//}
+//all();
